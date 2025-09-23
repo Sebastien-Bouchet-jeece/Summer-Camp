e34d48c355dd2464a03c7dcda31382f91c7935f4
Sebastien-Bouchet <sebastien.bouchet@edu.ece.fr>
2025-09-24 03:12:10 +0900

    delete post

--- client/src/components/Post/DeleteCard.js
+++ client/src/components/Post/DeleteCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { deletePost } from "../../actions/post.actions";
 
-const DeleteCard = () => {
+const DeleteCard = (props) => {
 
     const dispatch = useDispatch();
 
     const deleteQuote = () => {
-        
+        dispatch(deletePost(props.id));
     }
-    
+
   return (
     <div onClick={() => {
         if (window.confirm("Are you sure you want to delete this post?")) {
